fe929e41a93b95456d39fc26a25dad148c681a9a
Kevin Mitchell <kevin@Kevins-MBP.attlocal.net>
2020-04-04 18:15:14 -0500

    Add Barking Dog

---
+++ barking_dog/src/BarkingDog.java
@@ -0,0 +1,11 @@
+public class BarkingDog {
+
+  public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+    if(hourOfDay < 0 || hourOfDay > 23) {
+      return false;
+    } else if(barking == true && (hourOfDay < 8 || hourOfDay > 22)) {
+      return true;
+    }
+    return false;
+  }
+}
---
+++ barking_dog/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+  public static void main(String[] args) {
+    BarkingDog.shouldWakeUp(true, 1);
+    BarkingDog.shouldWakeUp(false, 2);
+    BarkingDog.shouldWakeUp(true, 8);
+    BarkingDog.shouldWakeUp(false, -1);
+  }
+}
